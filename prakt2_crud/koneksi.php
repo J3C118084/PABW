a4a5f031c19eb00dfd4181dfe64f7d70a9e00bca
trisharzk <trisharizki09@gmail.com>
2020-10-08 12:27:00 +0700

    Pert2

---
+++ prakt2_crud/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $host="sql211.epizy.com"; //"localhost";
+    $user="epiz_26901377"; //"root";
+    $pwd="14a3MbKeZa"; //"";
+    $dbname="epiz_26901377_trisha";//"pabw";
+    $kon=mysqli_connect($host,$user,$pwd,$dbname);
+?>
